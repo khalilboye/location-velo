46689c8eadf443f58d8b7f09fce4024216eeade9
ibrahima boye <khalilboye@gmail.com>
2020-05-10 04:05:45 +0000

    init

---
+++ src/main/java/com/sensoft/monbike/MonbikeApplication.java
@@ -0,0 +1,13 @@
+package com.sensoft.monbike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonbikeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonbikeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sensoft/monbike/MonbikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sensoft.monbike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonbikeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
